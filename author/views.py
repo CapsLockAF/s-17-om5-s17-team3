c2782db1c8314f85191db7cc3ed8c8a991e46a08
Karina1Karina <chobotar.karina@gmail.com>
2021-09-16 20:19:03 +0300

    add form to author

--- author/views.py
+++ author/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from author.models import Author
+from author.forms import AuthorForm
 
-# Create your views here.
+
+def all_authors(request):
+    authors=Author.get_all()
+    return render(request, "author/authors.html",
+                  {
+                      'title': 'Authors Information',
+                      "heading": "All Authors",
+                      'authors': authors,
+                  })
+
+
+def author_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            form = AuthorForm()
+        else:
+            authors = Author.objects.get(pk=id)
+            form = AuthorForm(instance=authors)
+        return render(request, "author/authors_form.html", {"form":form})
+    else:
+        if id == 0:
+            form = AuthorForm(request.POST)
+        else:
+            authors = Author.objects.get(pk=id)
+            form = AuthorForm(request.POST, instance=authors)
+        if form.is_valid():
+            form.save()
+        return redirect("/author")
+
+
+def delete_form(request, id):
+    Author.delete_by_id(id)
+    return redirect("/author")
--- library/urls.py
+++ library/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('book/', include('book.urls')),
+    path('author/', include('author.urls')),
     path('admin/', admin.site.urls),
 ]
