b422fa546ef31d00f2cb7291e0218b67d91b504f
o.fareniuk <fareniuk.olexandr@gmail.com>
2021-09-17 21:49:05 +0300

    add[User]: a form for creating user, methods, templates, urls, etc.

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_users, name='users'),
-    # path('<int:id>', views.view_user, name='user'),
-    # path('form/', views.user_form, name='user_form'),
-    # path('update/<int:id>', views.uodate_user_form, name='update_form'),
-    # path('delete/<int:id>', views.delete_user_form, name='delete_form'),
+    path('<int:id>/', views.view_user, name='view_user'),
+    path('form/<int:id>/', views.user_form, name='user_form'),
+    path('form/<int:id>/delete/', views.delete_user, name='delete_form'),
 ]
