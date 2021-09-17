61bc873836184dbfe5851655732ecbc927a49e55
Karina1Karina <chobotar.karina@gmail.com>
2021-09-17 14:52:55 +0300

    add form to book

--- author/urls.py
+++ author/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_authors, name='authors'),
-    path('form', views.author_form, name='author_form'),
+    path('form/', views.author_form, name='author_form'),
     path('form/<int:id>', views.author_form, name='update_form'),
     path('del/<int:id>', views.delete_form, name='delete_form'),
 ]
