d1a8d7e08506e3738451c012c41abff1f6b406c0
o.fareniuk <fareniuk.olexandr@gmail.com>
2021-09-17 16:07:02 +0300

    add[User]: base url in main settings, output list of users

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import UserForm
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+
+def all_users(request):
+    users = CustomUser.get_all()
+    return render(request, 'authentication/user_list.html',
+                  {
+                    'title': "Library Users",
+                    'heading': "List of all Users",
+                    "users": users,
+                   }
+                  )
