e20cdf4f4127d18fdffdb4ef5f403bcc3a6f62a2
Karina1Karina <chobotar.karina@gmail.com>
2021-09-18 20:24:48 +0300

    add rename deleting for author

--- author/urls.py
+++ author/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.all_authors, name='authors'),
     path('form/', views.author_form, name='author_form'),
     path('form/<int:id>', views.author_form, name='update_form'),
-    path('del/<int:id>', views.delete_form, name='delete_form'),
+    path('del/<int:id>', views.delete_author, name='delete_form'),
 ]
