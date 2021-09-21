237508e975b5d9d8c9949a32f295ce01a855c8d5
nosnosfik <nosfk@gmail.com>
2021-09-21 13:33:10 +0300

    Added api serializers/views for book and author apps

--- author/urls.py
+++ author/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('author', views.AuthorView)
 
 urlpatterns = [
     path('', views.all_authors, name='authors'),
--- book/urls.py
+++ book/urls.py
@@ -1,16 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+
+import author.views
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('book', views.BookView)
+router.register('author', author.views.AuthorView)
 
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
     path('', views.show_all_books, name='index'),
     path('form/', views.book_form, name='book_form'),
-    path('del/<int:id>', views.deleted_form, name='deleted_form'),
-    path('form/<int:id>', views.book_form, name='updated_form'),
-    path('ordered/', views.show_ordered_books, name='book'),
-    path('<int:book_id>/', views.show_book_by_id, name='book'),
-    path('sort/<str:field_name>/<str:state_sort>/', views.book_order, name='book'),
-    path('filter/<str:field_prop>/<str:field_value>', views.show_book_by_field, name='book'),
-    path('author/<int:author_id>/', views.show_book_by_author, name='book'),
-    path('start/', views.start, name='index'),
+    path('del/<int:id>/', views.deleted_form, name='deleted_form'),
+    path('form/<int:id>/', views.book_form, name='updated_form'),
+    path('ordered/', views.show_ordered_books, name='book_ordered'),
+    path('<int:book_id>/', views.show_book_by_id, name='book_by_id'),
+    path('sort/<str:field_name>/<str:state_sort>/', views.book_order, name='book_sort'),
+    path('filter/<str:field_prop>/<str:field_value>/', views.show_book_by_field, name='book_search'),
+    path('author/<int:author_id>/', views.show_book_by_author, name='book_by_author'),
+    path('start/', views.start, name='start'),
 ]
